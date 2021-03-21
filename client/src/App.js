d0cd9981e58c9dd118706adfd3136abb2cd5f9bd
Luiz Rolim <lrolim@twilio.com>
2021-03-21 15:05:28 -0300

    adjusting client logic

--- client/src/App.js
+++ client/src/App.js
@@ -1,64 +1,29 @@
 import { useEffect, useState } from 'react';
 
-const data = [
-  {
-    id: '1',
-    name: 'Pilsner',
-    minimumTemperature: 4,
-    maximumTemperature: 6,
-  },
-  {
-    id: '2',
-    name: 'IPA',
-    minimumTemperature: 5,
-    maximumTemperature: 6,
-  },
-  {
-    id: '3',
-    name: 'Lager',
-    minimumTemperature: 4,
-    maximumTemperature: 7,
-  },
-  {
-    id: '4',
-    name: 'Stout',
-    minimumTemperature: 6,
-    maximumTemperature: 8,
-  },
-  {
-    id: '5',
-    name: 'Wheat beer',
-    minimumTemperature: 3,
-    maximumTemperature: 5,
-  },
-  {
-    id: '6',
-    name: 'Pale Ale',
-    minimumTemperature: 4,
-    maximumTemperature: 6,
-  },
-];
+const statusString = (statusValue) => {
+  switch (statusValue) {
+    case -1: return "too low"
+    case 0: return "all good"
+    case 1: return "too high"
+  }
+}
+
+const POLLING_INTERVAL = 5000
 
 function App() {
   const [items, setItems] = useState({});
 
   useEffect(() => {
     const request = () =>
-      data.forEach((product) => {
-        fetch(`http://localhost:8081/temperature/${product.id}`)
-          .then((response) => response.json())
-          .then((response) =>
-            setItems((prevItems) => ({
-              ...prevItems,
-              [product.id]: {
-                ...product,
-                ...response,
-              },
-            }))
-          );
-      });
+      fetch(`http://localhost:8081/temperatures/`)
+        .then((response) => response.json())
+        .then((response) =>
+          setItems((prevItems) => ({
+            ...response
+          }))
+        );
 
-    setInterval(request, 5000);
+    setInterval(request, POLLING_INTERVAL);
 
     request();
   }, []);
@@ -80,14 +45,7 @@ function App() {
               <td width={150}>{items[itemKey].name}</td>
               <td width={150}>{items[itemKey].temperature}</td>
               <td width={150}>
-                {items[itemKey].temperature <
-                  items[itemKey].minimumTemperature && <span>too low</span>}
-                {items[itemKey].temperature >
-                  items[itemKey].maximumTemperature && <span>too high</span>}
-                {items[itemKey].temperature <=
-                  items[itemKey].maximumTemperature &&
-                  items[itemKey].temperature >=
-                    items[itemKey].minimumTemperature && <span>all good</span>}
+                <span>{statusString(items[itemKey].status)}</span>
               </td>
             </tr>
           ))}
@@ -96,5 +54,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
